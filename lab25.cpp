484e3715bbe1e0567577bcc435636d9cdd58708d
Nanashikung <nata.station@hotmail.com>
2022-02-15 18:40:06 +0700

    lab25.cpp complete

--- lab25.cpp
+++ lab25.cpp
@@ -29,6 +29,65 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.real == 0){
+		if(c.imag > 0) return os << c.imag << "i";
+		else if(c.imag == 0) return os << "0";
+		else return os  << c.imag << "i";
+	}else if(c.real > 0){
+		if(c.imag  > 0) return os << c.real << "+" << c.imag << "i";
+		else if(c.imag == 0) return os << c.real;
+		else return os  << c.real << c.imag << "i";
+	}else{
+		if(c.imag > 0) return os << c.real << "+" << c.imag << "i";
+		else if(c.imag == 0) return os << c.real;
+		else return os  << c.real << c.imag << "i";
+	}
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real-imag*c.imag,real*c.imag+imag*c.real);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double d = pow(c.real,2)+pow(c.imag,2);
+	return ComplexNumber((real*c.real-imag*-c.imag)/d,(real*-c.imag+imag*c.real)/d);
+}
+
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real-0*c.imag,s*c.imag+0*c.real);
+}
+
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	double d = pow(c.real,2)+pow(c.imag,2);
+	return ComplexNumber((s*c.real-0*c.imag)/d,(s*-c.imag+0*c.real)/d);
+}
+
+double ComplexNumber::abs(){
+	return pow((pow(real,2)+pow(imag,2)),1/2.0);
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag, real) / M_PI * 180;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag) return true;
+	else return false;
+}
+
+bool operator==(double s,const ComplexNumber &c){
+	if(s == c.real && s == c.imag) return true;
+	else return false;
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
@@ -69,6 +128,7 @@ int main(){
 	cout << (ComplexNumber(1,1) == ComplexNumber(1,2)) << "\n";
 	cout << (ComplexNumber(1,1) == 1) << "\n";
 	cout << (0 == ComplexNumber()) << "\n";
+	
 }
 
 
